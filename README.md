ca4844c50cb45c206dbe7aa76db7317d94cf7392
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-02-07 18:32:14 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# python-chef
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: python-chef
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
