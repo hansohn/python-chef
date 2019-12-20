55f392efb2fd84fc0ef9a93a8bacc795b6cf1cc6
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-12-19 21:33:51 -0800

    use native python

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,32 +1,29 @@
 default['python'] = {
   'python2' => {
-    'packages' => [
-      'python-devel',
-      'python2-pip',
-      'python-setuptools',
-    ],
+    'packages' => %w(
+      python-devel
+      python2-pip
+      python-setuptools
+    ),
     'bin' => '/bin/python2',
     'pip_bin' => '/bin/pip2',
     'easy_install_bin' => '/bin/easy_install-2.7',
-    'pips' => [],
-    'easy_installs' => [],
+    'pips' => %w(),
+    'easy_installs' => %w(),
     'symlinks' => {},
   },
   'python3' => {
-    'packages' => [
-      'python36u-devel',
-      'python36u-pip',
-      'python36u-setuptools',
-    ],
+    'packages' => %w(
+      python3-devel
+      python3-pip
+      python3-setuptools
+    ),
     'bin' => '/bin/python3.6',
     'pip_bin' => '/bin/pip3.6',
     'easy_install_bin' => '/bin/easy_install-3.6',
-    'pips' => [],
-    'easy_installs' => [],
-    'symlinks' => {
-      '/bin/python3' => 'python3.6',
-      '/usr/bin/python3' => 'python3.6',
-    },
+    'pips' => %w(),
+    'easy_installs' => %w(),
+    'symlinks' => {},
   },
   'virtualenvs' => {},
 }
