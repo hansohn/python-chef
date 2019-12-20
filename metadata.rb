73444a7a6776b3a50b2838bfe2896a9cd43bb2fc
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-12-19 21:30:06 -0800

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,9 @@ maintainer 'Ryan Hansohn'
 maintainer_email 'info@imnorobot.com'
 license 'MIT'
 description 'Installs/Configures python-chef'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
-chef_version '>= 13.0' if respond_to?(:chef_version)
-source_url 'https://github.com/hansohn/python-chef' if respond_to?(:source_url)
-issues_url 'https://github.com/hansohn/python-chef/issues' if respond_to?(:issues_url)
+version '0.2.0'
+chef_version '>= 13.0'
+source_url 'https://github.com/hansohn/python-chef'
+issues_url 'https://github.com/hansohn/python-chef/issues'
 
 supports 'centos', '>= 7.0'
